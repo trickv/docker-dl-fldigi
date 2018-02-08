6af8980ca9a89a275286bc5ff8ff30850616ae4f
Patrick van Staveren <trick@vanstaveren.us>
2018-02-08 23:47:11 +0000

    reshape arguments into different lines

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-docker run -v $HOME/.dl_fldigi:/root/.dl_fldigi -v /tmp/.X11-unix:/tmp/.X11-unix -ti -e DISPLAY=$DISPLAY dl-fldigi --hab
+docker run \
+    -v $HOME/.dl_fldigi:/root/.dl_fldigi \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -ti \
+    dl-fldigi --hab
