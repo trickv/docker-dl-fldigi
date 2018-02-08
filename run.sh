f44f2cbe0cf27fb01b2213a9bd7425e385816d51
Patrick van Staveren <trick@vanstaveren.us>
2018-02-08 23:44:29 +0000

    add --hab flag as noticed back in October

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v $HOME/.dl_fldigi:/root/.dl_fldigi -v /tmp/.X11-unix:/tmp/.X11-unix -ti -e DISPLAY=$DISPLAY dl-fldigi
+docker run -v $HOME/.dl_fldigi:/root/.dl_fldigi -v /tmp/.X11-unix:/tmp/.X11-unix -ti -e DISPLAY=$DISPLAY dl-fldigi --hab
