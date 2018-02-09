f54e90ab61762c61e25cd6ee14011faa28da9bbb
Patrick van Staveren <trick@vanstaveren.us>
2018-02-09 00:14:00 +0000

    attempt to get pulseaudio working. it's close.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 docker run \
     -v $HOME/.dl_fldigi:/root/.dl_fldigi \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /run/user/$(id -u)/pulse:/run/pulse:ro \
+    -v $HOME/.config/pulse/cookie:/root/.config/pulse/cookie:ro \
     -e DISPLAY=$DISPLAY \
     -ti \
     --rm \
